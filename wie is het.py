588b283e3fe922d4e242ce9af76e6562d0f8f6a8
unknown <99067235@mydavinci.nl>
2021-09-15 13:47:00 +0200

    update

---
+++ wie is het.py
@@ -0,0 +1,18 @@
+antwoord = input("is de kaas geel? Y/N").upper()
+
+if antwoord == "Y":
+    input("Zitten er gaten in?")
+    if antwoord == "Y":
+        input("Is de kaas belachelijk duur?")
+        if antwoord == "Y":
+            print("Emmenthaler")
+        if antwoord == "N":
+            print("Leerdammer")
+    if antwoord == "N":
+        input("Is de kaas hard als steen?")
+if antwoord == "N":
+    input("Heeft de kaas blauwe schimmels?")
+    if antwoord == "Y":
+        input("Heeft de kaas een korst?")
+    if antwoord == "N"
+        input("Heeft de kaas een korst?")
