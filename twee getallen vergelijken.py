b43a51fb46a83f66db765380decb96ae403b8289
unknown <99067235@mydavinci.nl>
2021-09-15 11:24:22 +0200

    'else-statement'

--- twee getallen vergelijken.py
+++ twee getallen vergelijken.py
@@ -3,7 +3,7 @@ b = int(input("Vul hier een getal in"))
 
 if a > b:
     print("a is het grootste getal:" + str(a))
-
 elif a < b:
     print("b is het grootste getal:" + str(b))
-    
+else:
+    print("a is gelijk aan b")
